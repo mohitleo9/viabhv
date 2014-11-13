5dec70612871ac56f1e0337913a14d4b40978382
maggarwal <maggarwal@amplify.com>
2014-11-13 15:39:36 -0500

    added a simple home.html

--- chat/chat/views/home.py
+++ chat/chat/views/home.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home_handler(request, asdf):
-    return HttpResponse("asdf")
+    return render(request, 'home.html', {'asdf':'asdf'})
