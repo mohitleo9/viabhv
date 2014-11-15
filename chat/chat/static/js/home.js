3aba53167ee30dda959b3f012c84d5d10fec670c
maggarwal <maggarwal@amplify.com>
2014-11-15 13:10:43 -0500

    making changes to home page - added jquery - added a form field and capture data on post

--- chat/chat/static/js/home.js
+++ chat/chat/static/js/home.js
@@ -1,1 +1,13 @@
 var asdf = 'asdf';
+
+
+// event registration
+$('#post').click( function(event) {
+    console.log("msg");
+    var message = $('#message').val();
+    // post the value if it exists
+    if (val){
+        console.log('msg');
+
+    }
+});
