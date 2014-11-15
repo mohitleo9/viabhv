eebbb61c4686a882fababb0ade539086936f4007
maggarwal <maggarwal@amplify.com>
2014-11-15 17:08:16 -0500

    added a message class

--- chat/chitchat/models.py
+++ chat/chitchat/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=10)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class Message(models.Model):
+    text = models.CharField(max_length=300)
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now_add=True)
